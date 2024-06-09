bbad16ec09ebdf518e1f0d390c9a0050ec850de7
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-09 12:37:12 +0400

    implement supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://avfqylrmzbocupzefphb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2ZnF5bHJtemJvY3VwemVmcGhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NjMxMTAsImV4cCI6MjAzMzQzOTExMH0.jGkH35v-v1g6u2EFHSP_m0SXmh_r3M5hzsQzsOQJAQo";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
