ec4d1c0607d4903b1fc560f5d394dc6e0e2c28dc
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-11 23:06:23 +0400

    implement outside detecting modal

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from "react";
+
+export function useOutsideClick(handler, listenCapturing = true) {
+  const ref = useRef();
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) handler();
+    }
+    document.addEventListener("click", handleClick, listenCapturing);
+
+    () => document.removeEventListener("click", handleClick, listenCapturing);
+  }, [handler, listenCapturing]);
+  return ref;
+}
