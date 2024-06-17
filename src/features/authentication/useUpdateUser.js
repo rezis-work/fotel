4860273085c54cc136eaa756eb777c0bbb84c9cc
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-06-17 22:03:07 +0400

    fix bug of apiAuth changing avatar

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -6,9 +6,10 @@ export function useUpdateUser() {
   const queryClient = useQueryClient();
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success("User account succesfully updated");
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.setQueryData(["user"], user);
+      // queryClient.invalidateQueries({ queryKey: ["user"] });
     },
     onError: (err) => toast.error(err.message),
   });
